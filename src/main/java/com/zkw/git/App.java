dd35b4407bc9974053cb53b056ae98ae26fb6fad
吃好喝好 <15384882265@163.com>
2023-05-05 18:15:51 +0800

    second commit

--- src/main/java/com/zkw/git/App.java
+++ src/main/java/com/zkw/git/App.java
@@ -3,5 +3,6 @@ package com.zkw.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("Hello Git 2 !!!!");
     }
 }
