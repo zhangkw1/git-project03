f7dfed71bed8ca36a053689a77406c0df0097d5a
吃好喝好 <15384882265@163.com>
2023-05-06 09:43:03 +0800

    hotfix first commit

--- src/main/java/com/zkw/git/App.java
+++ src/main/java/com/zkw/git/App.java
@@ -6,5 +6,6 @@ public class App {
         System.out.println("Hello Git 2 !!!!");
         System.out.println("Hello Git 3 !!!!!");
         System.out.println("Hello Git 1 hotfix first commit");
+        System.out.println("Hello Git 1 hotfix first modify");
     }
 }
